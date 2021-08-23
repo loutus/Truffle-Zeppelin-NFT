6ec7a37dcae2c1f2d494a63c5867dd411ff95f2c
r.nouripoor <r.nouripoor@gmail.com>
2021-08-23 17:58:01 +0430

    deployed perfectly on rinkeby

--- truffle-config.js
+++ truffle-config.js
@@ -4,6 +4,17 @@ const { mnemonic, projectId, MY_API_KEY } = require('./secret.json');
 
 module.exports = {
   networks: {
+    rinkeby: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${projectId}`)
+      },
+      network_id: "4",       // Rinkeby's id
+      gas: 8500000,        
+      gasPrice: 1000000000,  // 1 gwei (in wei) (default: 100 gwei)
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     ropsten: {
       provider: function() {
         return new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${projectId}`)
@@ -12,17 +23,6 @@ module.exports = {
       gas: 8500000,        
       gasPrice: 1000000000,
     }
-  },
-  rinkeby: {
-    provider: function() {
-      return new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${projectId}`)
-    },
-    network_id: 4,       // Rinkeby's id
-    gas: 8500000,        
-    gasPrice: 1000000000,  // 1 gwei (in wei) (default: 100 gwei)
-    confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-    timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
   },
   plugins: [
     'truffle-plugin-verify'
